e47b127a60b886f1308b44995bfa8490883ccc82
Evaldo Santos <jesantosjr@protonmail.com>
2021-10-20 20:52:26 -0300

    centralizando updates em um unico lugar

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -15,19 +15,17 @@ export class NegociacaoController {
     this.inputData = document.querySelector("#data");
     this.inputQuantidade = document.querySelector("#quantidade");
     this.inputValor = document.querySelector("#valor");
-
     this.negociacoesView.update(this.negociacoes);
   }
 
-  adiciona(): void {
+  public adiciona(): void {
     const negociacao = this.criaNegociacao();
     this.negociacoes.adiciona(negociacao);
-    this.negociacoesView.update(this.negociacoes);
-    this.mensagemView.update("Negociacao adicionada com sucesso");
     this.limparFormulario();
+    this.atualizaView();
   }
 
-  criaNegociacao(): Negociacao {
+  private criaNegociacao(): Negociacao {
     const exp = /-/g;
     const date = new Date(this.inputData.value.replace(exp, ','));
     const quantidade = parseInt(this.inputQuantidade.value, 10);
@@ -40,11 +38,16 @@ export class NegociacaoController {
     );
   }
 
-  limparFormulario(): void {
+  public limparFormulario(): void {
     this.inputData.value = '';
     this.inputQuantidade.value = '';
     this.inputValor.value = '';
 
     this.inputData.focus();
+  }
+
+  private atualizaView(): void {
+    this.negociacoesView.update(this.negociacoes);
+    this.mensagemView.update("Negociacao adicionada com sucesso");
   }
 }
