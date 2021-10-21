60b909678ecc143af64e514cf0c209d5b9496c66
Evaldo Santos <jesantosjr@protonmail.com>
2021-10-21 09:41:23 -0300

    metodos estaticos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,4 +14,17 @@ export class Negociacao {
     return this.quantidade * this.valor;
   }
 
+  public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+    const exp = /-/g;
+    const date = new Date(dataString.replace(exp, ','));
+    const quantidade = parseInt(quantidadeString, 10);
+    const valor = parseFloat(valorString);
+
+    return new Negociacao(
+      date, 
+      quantidade,
+      valor
+    );
+  }
+
 }
