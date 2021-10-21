037b3aade11fcbf3d4649f0125c47adab734cad8
Evaldo Santos <jesantosjr@protonmail.com>
2021-10-21 19:09:09 -0300

    api do decorator

--- app/decorators/logar-tempo-de-execucao.ts
+++ app/decorators/logar-tempo-de-execucao.ts
@@ -1,3 +1,9 @@
 export function logarTempoDeExecucao() {
-  
+  return function(
+    target: any,
+    propertykey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    return descriptor;
+  }
 }
